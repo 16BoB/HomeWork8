d7cebb85e43cbf74a92629f4a7e5395eea931510
Tape <trid.skivi@gmail.com>
2022-07-21 00:34:11 +0300

    formatted the code of the first task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,35 +8,45 @@
 // 2 3 5 9
 // 2 4 4 8
 
-int[,] CreateRandom2dArray (int rows, int columns, int minValue, int maxValue) {
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
     int[,] newArr = new int[rows, columns];
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            newArr[i,j] = new Random().Next(minValue, maxValue + 1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newArr[i, j] = new Random().Next(minValue, maxValue + 1);
         }
     }
     return newArr;
 }
 
-void Show2dArray (int[,] arr) {
-    for (int i = 0; i < arr.GetLength(0); i++) {
-        for (int j = 0; j < arr.GetLength(1); j++) {
-            Console.Write(arr[i,j] + " ");
+void Show2dArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
         }
         Console.WriteLine();
     }
 }
 
-int[,] SortingRowsFromLargest (int[,] arr) {
+int[,] SortingRowsFromLargest(int[,] arr)
+{
     int temp = 0;
-    for (int i = 0; i < arr.GetLength(0); i++) {
-        for (int j = 0; j < arr.GetLength(1); j++) {
-            for (int k = j + 1; k < arr.GetLength(1); k++) {
-                if (arr[i,j] < arr[i,k])
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < arr.GetLength(1); k++)
+            {
+                if (arr[i, j] < arr[i, k])
                 {
-                    temp = arr[i,k];
-                    arr[i,k] = arr[i,j];
-                    arr[i,j] = temp;
+                    temp = arr[i, k];
+                    arr[i, k] = arr[i, j];
+                    arr[i, j] = temp;
                 }
             }
         }
@@ -44,7 +54,7 @@ int[,] SortingRowsFromLargest (int[,] arr) {
     return arr;
 }
 
-int[,] myArr = CreateRandom2dArray (4, 4, 1, 9);
+int[,] myArr = CreateRandom2dArray(3, 4, 1, 9);
 
 Show2dArray(myArr);
 
